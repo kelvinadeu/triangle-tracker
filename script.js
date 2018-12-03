b020b6c796de0361c085e05a15a938568e023cf6
kelvinadeu <kelvinadeu25@gmail.com>
2018-12-03 14:15:36 +0300

    add to page html

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
-function triangle(){
-var sides =[];
-sides.push(document.getElementById("num1").value);
-sides.push(document.getElementById("num2").value);
-sides.push(document.getElementById("num3").value);
-var [a,b,c] = sides;
-var display = document.getElementById("display");
-var explanation = document.getElemntById("explanation");
-if(a.length === 0 || b.length === 0 || c.length === 0)
+
+function tracker(){
+  var a =parseInt(document.getElementbyId("one").value);
+  var b =parseInt(document.getElementbyId("two").value);
+  var c =parseInt(document.getEementbyId("three").value);
+
+  var sides=[];
+
+  sides.push("a");
+  sides.push("b");
+  sides.push("c");
+
+var results=document.querySelector("#final");
+
+ if(a+b<=c || b+c<=a || a+c<=b){
+  results.innerHTML="its not a triangle";
 }
-else{
-  a = parsefloat(a);
-  b = parsefloat(b);
-  c = parsefloat(c);
-  track(a,b,c);
+else if(a===b && b===c && c===a){
+  results.innerHTML="its Equilateral triangle";
 }
-function track (a,b,c){
-  if(a + b > c && b + c >a && c + a > b){
-    if(a=== b && a===c){
-      display.innerHTML = "<p class ='display -text'>That is an <span class='type'>Equilateral</span>triangle<p><span class='fill'>All</span>sides are equal";
-    }
-    else if(a===b || b===c || a===c){
-      display.innerHTML = "<p class ='display -text'>That is an <span class='type'>Isosceles</span>Triangle<p><span class='fill'>TWO</span>sides are equal";
-    }
-    else{
-      display.innerHTML = "<p class='display -text'>That is a <span class='type'>Scalene</span>triangle<p><span class='not'>NO</span>sides are equal";
-    }
-  }
-  else if(isNaN(a) || isNaN(b) || isNaN(c)){
-    display.innerHTML = "<p class='display-text'>That is a <span class='not'>NOT</span>a number</p>";
-  }
-  else{
-    display.innerHTML= "<p class='display-text'>That is <span class ='not'>NOT</span>a triangle</p><p>The sum of any two sides should be <span class='fill'>GREATER</span>Than the third side</p>"
-  }
+else if(b===a || a===c  || c===a){
+  results.innerHTML="its Isosceles triangle";
 }
-function Reset{
-  Location.reload();
+else if(a!===b && b!===c && c!===a){
+  results.innerHTML="its Scalene triangle";
+}
+}
+function refresh(){
+  location.reload();
 }
